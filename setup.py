d7cc7f3ef2b10b75ea833de195682eb5c8512e4a
Oski Bear <oski@bear>
2015-03-05 12:05:16 -0800

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy
+
+ext_modules = [
+  Extension(
+    name="ultratils.pysonix.scanconvert",
+    sources=["ultratils/pysonix/scanconvert.pyx"],
+    libraries = ["m"],
+    include_dirs=[numpy.get_include(), "."],
+    language="c",
+  )
+]
+
+
+setup(
+  name = 'ultratils',
+  cmdclass = {'build_ext': build_ext},
+  ext_modules = ext_modules,
+  packages = ['ultratils', 'ultratils.pysonix'],
+  package_data = {'ultratils.pysonix': ['data/probes.xml']},
+  scripts = [
+    'scripts/bpr2bmp',
+    'scripts/ultrasession.py'
+  ],
+  classifiers = [
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering'
+  ]
+)
