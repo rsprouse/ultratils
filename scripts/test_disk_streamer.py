93844e8a88e07bf06c56668faf4462a4edda95e2
Oski Bear <oski@bear>
2015-03-10 09:58:34 -0700

    Add utility scripts.

---
+++ scripts/test_disk_streamer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# Test use of disk_streamer.
+
+import ultratils.disk_streamer
+import subprocess
+import time
+
+streamer = ultratils.disk_streamer.DiskStreamer('test.wav')
+
+start = time.time()
+
+streamer.start_stream()
+
+proc = subprocess.Popen('./scripts/wait_for_input')
+print proc.poll()
+while proc.poll() is None:
+    time.sleep(0.1)
+
+streamer.stop_stream()
+streamer.close()
