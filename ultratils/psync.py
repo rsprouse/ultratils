e9975ecec0c38b9ef05611ce0321d7ee9df9d8bd
Ronald Sprouse <ronald@berkeley.edu>
2015-03-25 14:48:38 -0700

    Add psync.

---
+++ ultratils/psync.py
@@ -0,0 +1,67 @@
+import numpy as np
+import wave
+from contextlib import closing
+
+NORM_SYNC_THRESH = 0.2  # normalized threshold for detecting synchronizaton signal.
+MIN_SYNC_TIME = 0.0005   # minimum time threshold must be exceeded to detect synchronization signal
+                         # With pstretch unit sync signals are about 1 ms
+
+# From https://github.com/mgeier/python-audio/blob/master/audio-files/utility.py
+def pcm2float(sig, dtype=np.float64):
+    '''Convert integer pcm data to floating point.'''
+    sig = np.asarray(sig)  # make sure it's a NumPy array
+    assert sig.dtype.kind == 'i', "'sig' must be an array of signed integers!"
+    dtype = np.dtype(dtype)  # allow string input (e.g. 'f')
+    # Note that 'min' has a greater (by 1) absolute value than 'max'!
+    # Therefore, we use 'min' here to avoid clipping.
+    return sig.astype(dtype) / dtype.type(-np.iinfo(sig.dtype).min)
+
+def loadsync(wavfile, chan):
+    '''Load synchronization signal from an audio file channel and return as a normalized
+(range [-1 1]) 1D numpy array.'''
+    with closing(wave.open(wavfile)) as w:
+        nchannels = w.getnchannels()
+        assert w.getsampwidth() == 2
+        data = w.readframes(w.getnframes())
+        rate = w.getframerate()
+    sig = np.frombuffer(data, dtype='<i2').reshape(-1, nchannels)
+    return (pcm2float(sig[:,chan], np.float32), rate)
+
+def sync_pstretch(sig, threshold, min_run):
+    '''Find and return indexes of synchronization points from pstretch unit,
+defined as the start of a sequence of elements of at least min_run length,
+all of which are above threshold.'''
+    # Implementation: Create a boolean integer array where data points that
+    # exceed the threshold == 1, below == 0, then diff the boolean array.
+    # Sequences of true (above threshold) elements start where the diff == 1,
+    # and the sequence ends (falls below threshold) where the diff == -1.
+    # Circumfixing the array with zero values ensures there will be an equal
+    # number of run starts and ends even if the signal starts or ends with
+    # values above the threshold.
+    # TODO: auto threshold (%age of max?)
+    bounded = np.hstack(([0], sig, [0]))
+    thresh_sig = (bounded > threshold).astype(int)
+    difs = np.diff(thresh_sig)
+    run_starts = np.where(difs == 1)[0]
+    run_ends = np.where(difs == -1)[0]
+    return run_starts[np.where((run_ends - run_starts) > min_run)[0]]
+
+def sync2text(wavname, chan):
+    '''Find the synchronization signals in an acquisition's .wav file and
+create a text file that contains frame numbers and time stamps for each pulse.
+
+chan = channel number where sync signal is found (0 == first channel)
+'''
+    (syncsig, rate) = loadsync(wavname, chan)
+    syncsamp = sync_pstretch(syncsig, NORM_SYNC_THRESH, MIN_SYNC_TIME * rate)
+    synctimes = np.round(syncsamp * 1.0 / rate, decimals=4)
+    print "Found {0:d} synchronization pulses.".format(len(syncsamp))
+    dtimes = np.diff(synctimes)
+    print "Frame durations range [{0:1.4f} {1:1.4f}].".format(dtimes.min(), dtimes.max())
+    txtname = wavname.replace('.wav', '.sync.txt')
+    with open(txtname, 'w') as fout:
+        for idx,t in enumerate(synctimes):
+            fout.write("{0:0.4f}\t{1:d}\n".format(t,idx))
+ 
+
+
