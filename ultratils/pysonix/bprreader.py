bbd2502c0ef91e3c164b6476ce677d45754f6f78
Ronald Sprouse <ronald@berkeley.edu>
2015-05-20 12:55:54 -0700

    Add checksumming.

--- ultratils/pysonix/bprreader.py
+++ ultratils/pysonix/bprreader.py
@@ -2,6 +2,7 @@
 
 import struct
 import numpy as np
+import hashlib
 
 class Header(object):
     def __init__(self, filehandle):
@@ -24,7 +25,7 @@ class Header(object):
                 idx += 1
 
 class BprReader:
-    def __init__(self, filename):
+    def __init__(self, filename, checksum=False):
         self._fhandle = open(filename, 'rb')
         self.header = Header(self._fhandle)
         if self.header.filetype != 2:
@@ -33,11 +34,25 @@ class BprReader:
         # data_fmt and framesize values are specific to .bpr
         self.data_fmt = 'B' * (self.header.h * self.header.w)
         self.framesize = 1 * (self.header.h * self.header.w)
+        self.csums = [None] * self.header.nframes
+        if checksum:
+            for idx in range(self.header.nframes):
+                data = self.get_frame()
+                #self.csums[idx] = hashlib.sha1(data.copy(order="c")).hexdigest()
+                self.csums[idx] = hashlib.sha1(data).hexdigest()
+        self._fhandle.seek(self.header.packed_size)
+
  
-    def get_frame(self):
-        '''Get the next image frame.'''
+    def get_frame(self, idx=None, advance=True):
+        '''Get the next image frame. If idx is provided, read frame of given index. By default the file handle's current position advances to the end of the read frame. If advance is False the file handle's current position remains unchanged by the read().'''
 # TODO: do something appropriate if there is no more data to read
+        if not advance:
+            cur_pos = self._fhandle.tell()
+        if idx is not None:
+            self._fhandle.seek(self.header.packed_size + (idx * self.framesize))
         packed_data = self._fhandle.read(self.framesize)
+        if not advance:
+            self._fhandle.seek(cur_pos)
         data = np.array(struct.unpack(self.data_fmt, packed_data))
         return data.reshape([self.header.w, self.header.h]).T
 
