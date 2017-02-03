6410d1b6886f8de9c010808722ec180fc89a34ed
Ronald L. Sprouse <ronald@berkeley.edu>
2017-02-02 16:00:34 -0800

    Temporarily remove scanconvert.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+#from Cython.Distutils import build_ext
 import numpy
 
 ext_modules = [
